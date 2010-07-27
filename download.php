f67c7dcc29bbc379da81c83ee41bab254ac151af
MrMagic <eliacereda+github@gmail.com>
2010-07-27 13:19:03 +0200

    Added "download.php" file for downloading from Safari Gallery

---
+++ download.php
@@ -0,0 +1,5 @@
+<?php
+header("Content-type: application/x-safari-extension");
+header("Content-Disposition: attachment; filename=GMail Counter.safariextz");
+readfile("GMail Counter.safariextz");
+?>
