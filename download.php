ff636557fa8bb2f284b466412c35cb2af7ffc895
MrMagic <eliacereda+github@gmail.com>
2010-09-09 14:27:54 +0200

    Removed old files

--- download.php
+++
@@ -1,5 +0,0 @@
-<?php
-header("Content-type: application/x-safari-extension");
-header("Content-Disposition: attachment; filename=GMail Counter.safariextz");
-readfile("GMail Counter.safariextz");
-?>
