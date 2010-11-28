8f3fc3189d87211f95a4a8b536676a424c8bd171
MrMagic <eliacereda+github@gmail.com>
2010-11-28 17:56:05 +0100

    Released 0.8 | beta 2...
    Release notes:
    
    • Fixed Windows compatibility issues
    • Minor code improvements
    • Beautiful new icon, thanks to Bart Kowalski (http://www.bartkowalski.com)

--- GMail Counter.safariextension/js/GMailCounter.js
+++ GMail Counter.safariextension/js/GMailCounter.js
@@ -21,77 +21,23 @@ To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-
 	Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
 */
 GMailCounter = {
-	humanVersion: "0.8 | beta 1",
-	version: "8.b.1",
+	humanVersion: "0.8 | beta 2",
+	version: "8.b.2",
 	releaseType: "publicBeta",
 	homepage: "http://elix14.altervista.org/",
-	motdURL: "api/motd.php",
+	newsURL: "api/feed.php?v=1",
 	
 	tracker: {}, //This will contain the ExtTracker object
-	trackerId: "---", //ExtTracker extension ID
+	trackerId: "GMailCounter", //ExtTracker extension ID
 	
 	init: function() {
 		GMailCounter.tracker = new ExtTracker(GMailCounter.trackerId, GMailCounter.version);
-		GMailCounter.motd();
+		
+		ExtNews.load(GMailCounter.homepage+GMailCounter.newsURL);
 	},
 	
 	log: function(message) {
 		GMailCounter.tracker.logEvent(message);
-	},
-	
-	motd: function() {
-		var xhr = new XMLHttpRequest();
-		xhr.onreadystatechange = function() {
-			if(xhr.readyState == 4 && xhr.status) {
-				var obj = JSON.parse(xhr.responseText);
-				
-				obj.forEach(function(element) {
-					var AmITarget;
-					element.targets.forEach(function(target) {
-						switch ( target ) {
-							case GMailCounter.version:
-							case GMailCounter.releaseType:
-							case "everyone":
-								AmITarget = true;
-							break;
-						}
-					});
-					
-					if(AmITarget) {
-						var toBeShownNow;
-						switch ( element.toBeShown ) {
-							//case "once":\\	TO
-							//	null	  \\	DO
-							//break;	  \\
-							case "onEveryUpdate":
-								var state = GMailCounter.tracker.getState();
-								if(state != "started") {
-									toBeShownNow = true;
-								}
-							break;
-							case "onEveryStartup":
-								toBeShownNow = true;
-							break;
-						}
-						if(toBeShownNow) {
-							element.actions.forEach(function(a) {
-								switch ( a.type ) {
-									case "OpenURL":
-										safari.application.openBrowserWindow().activeTab.url = a.url;
-									break;
-									
-									case "ShowAlert":
-										alert(a.text);
-									break;
-								}
-							});
-						}
-					}
-				});
-			}
-		};
-		xhr.open("GET", GMailCounter.homepage+GMailCounter.motdURL, true);
-		xhr.send();
 	}
 };
 
