62fe02b85fdc9f6d4c15dacb1a021d017f951f08
MrMagic <eliacereda+github@gmail.com>
2011-02-25 13:18:03 +0100

    Fixed a bug in version.js
    Repacked

--- GMail Counter.safariextension/js/include/version.js
+++ GMail Counter.safariextension/js/include/version.js
@@ -12,9 +12,9 @@
     }
     
     window.v = function (str) {
-        var match = str.match(/([0-9]+).(a|b).([0-9]+)$/i);
+        var match = str.match(/([0-9]+)\.(a|b)\.([0-9]+)$/i);
         if (match) {
-            str = str.replace(/([0-9]+).(a|b).([0-9]+)$/i, ((match[2] === "a") ? -1e+6 : -1e+12) + Number(match[1]) - Number(match[3]));
+            str = str.replace(/([0-9]+)\.(a|b)\.([0-9]+)$/i, ((match[2] === "a") ? -1e+6 : -1e+12) + Number(match[1]) - Number(match[3]));
         }
         
         var parts = str.split(".");
