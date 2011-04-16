cd403482dab714dccde690118218756b02dc9423
Frank Kohlhepp <frnkkpp@googlemail.com>
2011-04-16 17:26:32 +0200

    Updated store.js

--- GMail-Counter.safariextension/frameworks/Utilities.js
+++ GMail-Counter.safariextension/frameworks/Utilities.js
@@ -1,19 +1,115 @@
-/*
-// Store-js
+//
 // Copyright (c) 2011 Frank Kohlhepp
 // https://github.com/frankkohlhepp/store-js
 // License: MIT-license
-*/
+//
+// Contains a small and customized part of MooTools
+// Copyright (c) 2006-2010 Valerio Proietti
+// http://mootools.net/
+// License: MIT-license
+//
 (function () {
+    Array.prototype.$family = function () { return "array"; };
+    
+    function typeOf(item) {
+        if (item === null) {
+            return "null";
+        }
+        
+        if (item.$family) {
+            return item.$family();
+        }
+        
+        return typeof item;
+    };
+    
+    function cloneObject(object) {
+        var clone = {};
+        for (var key in object) {
+            clone[key] = cloneItem(object[key]);
+        }
+        
+        return clone;
+    }
+    
+    function cloneArray(item) {
+        var i = item.length,
+            clone = new Array(i);
+        
+        while (i--) {
+            clone[i] = cloneItem(item[i]);
+        }
+        
+        return clone;
+    }
+    
+    function cloneItem(item) {
+        if (typeOf(item) === "array") {
+            return cloneArray(item);
+        } else if (typeOf(item) === "object") {
+            return cloneObject(item);
+        } else {
+            return item;
+        }
+    }
+    
+    function merge() {
+        var target = cloneObject(arguments[0]),
+            original = cloneObject(arguments[0]);
+        
+        for (var i = 1; i < arguments.length; i++) {
+            var object = arguments[i];
+            
+            // Remove keys from the target, that were removed
+            for (var key in original) {
+                if (original.hasOwnProperty(key)) {
+                    if (!object.hasOwnProperty(key)) {
+                        delete target[key];
+                    } else if (typeof object[key] === "object" && object[key] !== null) {
+                        target[key] = merge(original[key], object[key]);
+                    }
+                }
+            }
+            
+            // Merge the rest
+            for (var key in object) {
+                if (object.hasOwnProperty(key)) {
+                    if (typeof object[key] === "object" && object[key] !== null) {
+                        target[key] = merge(original[key], object[key]);
+                    } else if (object[key] !== original[key]) {
+                        target[key] = object[key];
+                    }
+                }
+            }
+        }
+        
+        return target;
+    }
+    
     this.Store = function (name) {
-        // Prototype that contains the two methods
+        // Creates a new prototype for every store
+        // because the name variable is transmitted
+        // via a closure.
         var storePrototype = {
             "save": function () {
-                var stringifiedObj = JSON.stringify(this);
-                localStorage.setItem(name, stringifiedObj);
+                var newStorage = localStorage.getItem(name);
+                if (newStorage !== originalStorage) {
+                    originalStorageObj = JSON.parse(originalStorage || "{}");
+                    newStorage = JSON.parse(newStorage || "{}");
+                    
+                    var save = merge(originalStorageObj, newStorage, this);
+                } else {
+                    var save = this;
+                }
                 
-                if (localStorage.getItem(name) !== stringifiedObj) {
-                    throw "saveFailed";
+                try {
+                    localStorage.setItem(name, JSON.stringify(save));
+                } catch (e) {
+                    if (e.code === 22) {
+                        throw "quotaExceeded";
+                    } else {
+                        throw "unknownError"
+                    }
                 }
                 
                 return this;
@@ -22,7 +118,7 @@
             "remove": function () {
                 localStorage.removeItem(name);
                 
-                // Remove any Members from this Object
+                // Restore to a clean store
                 for (var key in this) {
                     if (this.hasOwnProperty(key)) {
                         delete this[key];
@@ -33,7 +129,9 @@
             }
         };
         
-        var store = JSON.parse(localStorage.getItem(name) || "{}");
+        // Save the current state in localStorage
+        var originalStorage = localStorage.getItem(name),
+            store = JSON.parse(localStorage.getItem(name)  || "{}");
         store.__proto__ = storePrototype;
         return store;
     };
