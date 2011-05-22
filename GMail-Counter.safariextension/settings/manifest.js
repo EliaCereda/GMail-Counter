4f61de6842c19438031b598ae1a6fa9c3a1108b2
Frank Kohlhepp <frnkkpp@googlemail.com>
2011-05-22 19:00:55 +0200

    Cleaned manifest.js and settings.js

--- GMail-Counter.safariextension/settings/manifest.js
+++ GMail-Counter.safariextension/settings/manifest.js
@@ -1,184 +1,167 @@
 this.manifest = {
     "name": "GMail Counter - Settings",
-	"icon": "../Icon.png",
+    "icon": "../Icon.png",
     "settings": [
-	    {
-			"tab": "General",
-			"group": "Behavior",
-			
-	        "name": "Behavior_label",
-	        "type": "text",
-	        "label": "Gmail Label",
-	        "text": "i.e. \"All\" or \"Inbox\""
-	    },
-	    {
-			"tab": "General",
-			"group": "Behavior",
-		
-	        "name": "Behavior_openLinksIn",
-	        "type": "radioButtons",
-	        "label": "Open links in:",
-	        "options": [
-	            {
-	                "value": "GmailTab",
-	                "text": "any tab already opened on a Gmail page"
-	            },
-	            {
-	                "value": "activeTab",
-					"text": "the active tab"
-	            },
-				{
-	                "value": "newTab",
-					"text": "a new tab"
-	            },
-				{
-	                "value": "newWindow",
-					"text": "a new window"
-	            }
-	        ]
-	    },
-	
-	    {
-			"tab": "General",
-			"group": "Google Apps",
-		
+        {
+            "tab": "General",
+            "group": "Behavior",
+            
+            "name": "Behavior_label",
+            "type": "text",
+            "label": "Gmail Label",
+            "text": "i.e. \"All\" or \"Inbox\""
+        },
+        {
+            "tab": "General",
+            "group": "Behavior",
+            "name": "Behavior_openLinksIn",
+            "type": "radioButtons",
+            "label": "Open links in:",
+            "options": [
+                {
+                    "value": "GmailTab",
+                    "text": "any tab already opened on a Gmail page"
+                },
+                {
+                    "value": "activeTab",
+                    "text": "the active tab"
+                },
+                {
+                    "value": "newTab",
+                    "text": "a new tab"
+                },
+                {
+                    "value": "newWindow",
+                    "text": "a new window"
+                }
+            ]
+        },
+        {
+            "tab": "General",
+            "group": "Google Apps",
             "name": "GoogleApps_enable",
             "type": "checkbox",
             "label": "Enable"
         },
         {
-			"tab": "General",
-			"group": "Google Apps",
-	
+            "tab": "General",
+            "group": "Google Apps",
             "name": "GoogleApps_domain",
             "type": "text",
             "label": "Domain",
             "text": "i.e. example.com"
         },
-
-		{
-			"tab": "Notifications",
-			"group": "Sounds",
-		
+        {
+            "tab": "Notifications",
+            "group": "Sounds",
             "name": "Sounds_enable",
             "type": "checkbox",
             "label": "Enable"
         },
         {
-			"tab": "Notifications",
-			"group": "Sounds",
-		
+            "tab": "Notifications",
+            "group": "Sounds",
             "name": "Sounds_volume",
             "type": "slider",
             "label": "Volume",
             "max": 1,
-			"min": 0,
-			"step": 0.01
+            "min": 0,
+            "step": 0.01
         },
-		{
-			"tab": "Notifications",
-			"group": "Sounds",
-		
+        {
+            "tab": "Notifications",
+            "group": "Sounds",
             "name": "Sounds_Non-Breaking_SPace",
             "type": "description",
             "text": ""
         },
-		{
-			"tab": "Notifications",
-			"group": "Sounds",
-		
+        {
+            "tab": "Notifications",
+            "group": "Sounds",
             "name": "Sounds_description",
             "type": "description",
             "text": "You can choose any audio file QuickTime can open with a max. size of 512KB."
         },
-		{
-			"tab": "Notifications",
-			"group": "Sounds",
-		
+        {
+            "tab": "Notifications",
+            "group": "Sounds",
             "name": "Sounds_name",
             "type": "text",
             "label": "Current sound",
             "text": "loading..."
         },
-		{
-			"tab": "Notifications",
-			"group": "Sounds",
-		
+        {
+            "tab": "Notifications",
+            "group": "Sounds",
             "name": "Sounds_choose",
             "type": "button",
             "text": "Choose another sound..."
         },
-
-		{
-			"tab": "Notifications",
-			"group": "Head Viewer",
-		
+        {
+            "tab": "Notifications",
+            "group": "Head Viewer",
             "name": "HeadViewer_description",
             "type": "description",
             "text": "(a.k.a. the toolbar under Safari's favorites bar)"
         },
         {
-			"tab": "Notifications",
-			"group": "Head Viewer",
-		
+            "tab": "Notifications",
+            "group": "Head Viewer",
             "name": "HeadViewer_interval",
             "type": "popupButton",
             "label": "Show each email for",
-			"options": [
+            "options": [
                 {
                     "value": "2",
                     "text": "2 seconds"
                 },
                 {
                     "value": "5",
-					"text": "5 seconds"
+                    "text": "5 seconds"
                 },
-				{
+                {
                     "value": "10",
-					"text": "10 seconds"
+                    "text": "10 seconds"
                 },
-				{
+                {
                     "value": "15",
-					"text": "15 seconds"
+                    "text": "15 seconds"
                 },
-				{
+                {
                     "value": "20",
-					"text": "20 seconds"
+                    "text": "20 seconds"
                 },
-				{
+                {
                     "value": "30",
-					"text": "30 seconds"
+                    "text": "30 seconds"
                 },
-				{
+                {
                     "value": "45",
-					"text": "45 seconds"
+                    "text": "45 seconds"
                 },
-				{
+                {
                     "value": "60",
-					"text": "a minute"
+                    "text": "a minute"
                 }
             ]
         },
         {
-			"tab": "Notifications",
-			"group": "Head Viewer",
-		
+            "tab": "Notifications",
+            "group": "Head Viewer",
             "name": "HeadViewer_autoHide",
             "type": "checkbox",
             "label": "Close the Head Viewer when there is nothing to show"
         },
-		{
-			"tab": "Notifications",
-			"group": "Head Viewer",
-		
+        {
+            "tab": "Notifications",
+            "group": "Head Viewer",
             "name": "HeadViewer_Non-Breaking_SPace",
             "type": "description",
             "text": ""
         },
         {
-			"tab": "Notifications",
-			"group": "Head Viewer",
-		
+            "tab": "Notifications",
+            "group": "Head Viewer",
             "name": "HeadViewer_closeBehavior",
             "type": "radioButtons",
             "label": "Clicking on the \"close\" button will:",
@@ -189,6 +172,5 @@ this.manifest = {
                 }
             ]
         }
-
     ]
-}
+};
--- GMail-Counter.safariextension/settings/settings.js
+++ GMail-Counter.safariextension/settings/settings.js
@@ -1,32 +1,15 @@
 window.addEvent("domready", function () {
-    // Option 1: Use the manifest:
-    
-	new FancySettings.initWithManifest(function (settings) {
-		console.log(settings);
-		settings.manifest.Sounds_name.element.readOnly = true;	//Set sound name to read-only, as it will be set on sound change
-		settings.manifest.Sounds_choose.addEvent("action", function(e){
-			
-			if (settings.manifest.Sounds_choose.element.value != "OK") {
-				settings.manifest.Sounds_choose.element.value = "OK";
-			} else {
-				settings.manifest.Sounds_choose.element.value = settings.manifest.Sounds_choose.params.text;
-			}
-			alert("This is still TODO (issue #5)...");
-		});
-	});
-    
-    
-    
-    // Option 2: Do everything manually:
-    
-    // var settings = new FancySettings("My Extension");
-    // 
-    // var checkbox1 = settings.create("tabName", "groupName", "checkbox", {
-    //     "name": "checkbox1",
-    //     "label": "Enable this"
-    // });
-    // 
-    // checkbox1.addEvent("action", function (value) {
-    //     alert("you toggled me: " + value);
-    // });
+    new FancySettings.initWithManifest(function (settings) {
+        console.log(settings);
+        settings.manifest.Sounds_name.element.readOnly = true;  //Set sound name to read-only, as it will be set on sound change
+        settings.manifest.Sounds_choose.addEvent("action", function(e){
+            
+            if (settings.manifest.Sounds_choose.element.value != "OK") {
+                settings.manifest.Sounds_choose.element.value = "OK";
+            } else {
+                settings.manifest.Sounds_choose.element.value = settings.manifest.Sounds_choose.params.text;
+            }
+            alert("This is still TODO (issue #5)...");
+        });
+    });
 });
