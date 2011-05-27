2b8fe90ec2ee711b513e46fbd18344b377d284bb
Frank Kohlhepp <frnkkpp@googlemail.com>
2011-05-27 20:06:49 +0200

    Added more missing german translations

--- GMail-Counter.safariextension/js/i18n.js
+++ GMail-Counter.safariextension/js/i18n.js
@@ -8,43 +8,43 @@ this.i18n = {
 
     "newMessage": {
         "en": "new message",
-        "de": "",
+        "de": "neue Nachricht",
 		"it": "mail non letta"
     },
 
     "newMessages": {
         "en": "new messages",
-        "de": "",
+        "de": "neue Nachrichten",
 		"it": "mail non lette"
     },
 
 	"noNewMessages": {
         "en": "no new messages",
-        "de": "",
+        "de": "keine neue Nachrichten",
 		"it": "nessuna mail non letta"
     },
 
 	"NoNewMessages": {
         "en": "No new messages",
-        "de": "",
+        "de": "Keine neue Nachrichten",
 		"it": "Nessuna mail non letta"
     },
 
 	"noSubject": {
         "en": "[no subject]",
-        "de": "",
+        "de": "[kein Betreff]",
 		"it": "[nessun oggetto]"
     },
 
 	"ClickToLogin": {
         "en": "Click here to login",
-        "de": "",
+        "de": "Klicken zum Einloggen",
 		"it": "Clicca qui per accedere"
     },
 
 	"ErrorOccurred": {
         "en": "An error occurred, click here to re-login...",
-        "de": "",
+        "de": "Es ist ein Fehler aufgetreten, zum erneuten Einloggen hier klicken...",
 		"it": "Errore sconosciuto, clicca qui per ri-accedere..."
     }
 };
